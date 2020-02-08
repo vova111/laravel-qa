24b4afde18e7fef998cfabcd65923063840c2ff6
Vladimir Skirga <vladimir.skirga84@gmail.com>
2020-02-08 23:15:22 +0200

    display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    protected $fillable = ['body', 'user_id'];
+
     public static function boot()
     {
         parent::boot();
