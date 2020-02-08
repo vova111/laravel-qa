55e68a09e384961a87dd1f8f697bab271e8263f5
Vladimir Skirga <vladimir.skirga84@gmail.com>
2020-02-09 00:13:56 +0200

    update best_answer_id on questions table when an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -17,7 +17,13 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+
+            if ($question->best_answer_id == $answer->id) {
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
     }
 
@@ -39,5 +45,10 @@ class Answer extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
