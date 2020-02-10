ef92095909cbfb47f6772431fd0a726853e0bcd8
Vladimir Skirga <vladimir.skirga84@gmail.com>
2020-02-10 16:56:59 +0200

    Implement vue.js custom events on the answer deletion

--- public/js/app.js
+++ public/js/app.js
@@ -11261,11 +11261,7 @@ __webpack_require__.r(__webpack_exports__);
         position: 'center',
         buttons: [['<button><b>YES</b></button>', function (instance, toast) {
           axios["delete"](_this2.endpoint).then(function (res) {
-            $(_this2.$el).fadeOut(500, function () {
-              _this2.$toast.success(res.data.message, 'Success', {
-                timeout: 3000
-              });
-            });
+            _this2.$emit('deleted');
           });
           instance.hide({
             transitionOut: 'fadeOut'
@@ -11364,6 +11360,10 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
         _this.nextUrl = data.next_page_url;
       });
+    },
+    remove: function remove(index) {
+      this.answers.splice(index, 1);
+      this.count--;
     }
   }
 });
@@ -47793,10 +47793,15 @@ var render = function() {
                 _vm._v(" "),
                 _c("hr"),
                 _vm._v(" "),
-                _vm._l(_vm.answers, function(answer) {
+                _vm._l(_vm.answers, function(answer, index) {
                   return _c("answer", {
                     key: answer.id,
-                    attrs: { answer: answer }
+                    attrs: { answer: answer },
+                    on: {
+                      deleted: function($event) {
+                        return _vm.remove(index)
+                      }
+                    }
                   })
                 }),
                 _vm._v(" "),
--- resources/js/components/Answers.vue
+++ resources/js/components/Answers.vue
@@ -8,7 +8,7 @@
           </div>
           <hr>
 
-          <answer v-for="answer in answers" :answer="answer" :key="answer.id"></answer>
+          <answer @deleted="remove(index)" v-for="(answer, index) in answers" :answer="answer" :key="answer.id"></answer>
 
           <div class="text-center mt-3" v-if="nextUrl">
             <button @click.prevent="fetch(nextUrl)" class="btn btn-outline-secondary">Load more answers</button>
@@ -50,6 +50,10 @@
             this.answers.push(...data.data);
             this.nextUrl = data.next_page_url;
           });
+      },
+      remove (index) {
+        this.answers.splice(index, 1);
+        this.count--;
       },
     },
   };
