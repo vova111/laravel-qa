15ec316462724f2fdbb43653d1ec2fbf137a91e1
Vladimir Skirga <vladimir.skirga84@gmail.com>
2020-02-14 17:32:05 +0200

    Create api endpoints for vote question and answer

---
+++ app/Http/Controllers/Api/VoteQuestionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Question;
+
+class VoteQuestionController extends Controller
+{
+    public function __invoke(Question $question)
+    {
+        $vote = (int) request()->vote;
+
+        $votesCount = auth()->user()->voteQuestion($question, $vote);
+
+        return response()->json([
+            'message' => 'Thanks for the feedback',
+            'votesCount' => $votesCount,
+        ]);
+    }
+}
